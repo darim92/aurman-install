f7613f77fbb5a6c4dc241736c6a2a819e35d42b3
Darim <dario.caroprese@outlook.de>
2018-06-20 20:45:23 +0200

    Update aurman_install.sh

--- aurman_install.sh
+++ aurman_install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # make temp dir
-mkdir -p /tmp/pacaur_install
+mkdir -p /tmp/aurman_install
 cd /tmp/aurman_install
 
 echo "installing dependencies..."
 
 # install dependencies
-sudo pacman -S binutils make gcc fakeroot pkg-config --noconfirm --needed
+sudo pacman -Sy binutils make gcc fakeroot pkg-config --noconfirm --needed
 sudo pacman -S git python python-regex python-requests ---noconfirm --needed
 
 echo "installing expac-git..."
 
 # install expac-git
 curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=expac-git
-makepkg PKGBUILD --skippgpcheck --install --needed
+makepkg PKGBUILD --skippgpcheck --install --noconfirm --needed
 
 echo "installing expac-git..."
 
 # install aurman
 curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=aurman
-makepkg PKGBUILD --skippgpcheck --install --needed
+makepkg PKGBUILD --skippgpcheck --install --noconfirm --needed
 
 # clean up
 cd ~
-rm -r /tmp/aurman_install
+rm -r -f /tmp/aurman_install
 echo "enjoy aurman!"
 echo "-------------"
 
